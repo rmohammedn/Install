624847baf73fd89c8b1aa811e066b31fe40c7f42
mohammed <rmohammedn94@gmail.com>
2019-10-24 07:50:06 +0530

    adding atom.sh

---
+++ atom.sh
@@ -0,0 +1,6 @@
+chmod 777 atom.sh
+
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+sudo apt-get install atom
